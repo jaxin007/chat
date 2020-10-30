b49d219df88f5bfde34011b56b11884c738d78cf
jaxin007 <swatdimonbil@gmail.com>
2020-10-30 16:37:09 +0200

    chat-1 fixed message service interface

--- src/interfaces/messageServiceInterface.ts
+++ src/interfaces/messageServiceInterface.ts
@@ -3,9 +3,23 @@ import mongoose from 'mongoose';
 import {
   Message,
   NewMessage,
-} from '../models/messageModel';
+  RoomModel,
+} from '../models';
 
 export interface MessageServiceInterface {
-  addMessage(username: string, message: NewMessage): Promise<mongoose.Document>;
-  getMessages(offset?: number): Promise<Message[] | mongoose.Document[]>;
+  addMessage(
+    username: string,
+    message: NewMessage,
+    messageId: string,
+  ): Promise<mongoose.DocumentQuery<null | mongoose.Document, mongoose.Document>>
+
+  createRoom(roomName: string): Promise<mongoose.Document>;
+
+  findRoom(roomName: string): Promise<mongoose.Document | null>;
+
+  findRooms(): Promise<RoomModel[] | mongoose.Document[]>
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  findMessages(offset?: number, roomId: string): Promise<Message[] | mongoose.Document[]>
 }
